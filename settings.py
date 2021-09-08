d996b688125c9bc8a2b07a460abfa8e4dccb614d
Siti Khotijah <53899191+skhotijah@users.noreply.github.com>
2021-09-08 22:38:38 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,4 @@
+DEFAULT_CONFIDENCE_THRESHOLD = 0.5
+DEMO_IMAGE = "images/demo.jpg"
+MODEL = r"C:\Users\skhot\Streamlit\data/MobileNetSSD_deploy.caffemodel"
+PROTOTXT = r"C:\Users\skhot\Streamlit\data/MobileNetSSD_deploy.prototxt.txt"
